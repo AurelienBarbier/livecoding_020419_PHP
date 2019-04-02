f6bb7adc70a371ae23c8b57845edbd696f30b218
AurelienBarbier <aurelien@wildcodeschool.fr>
2019-04-02 15:50:45 +0200

    Fin livecoding

--- public/index.php
+++ public/index.php
@@ -6,6 +6,24 @@ require_once('../src/functions.php');
 $pdo = new PDO(DSN, LOGIN, PWD);
 
 
+if (!empty($_POST)) {
+
+    $sqlInsert = 'INSERT INTO contact (`lastname`, `firstname`, `civility_id`) 
+              VALUES (:lastname, :firstname, :civility)';
+
+    $prepared = $pdo->prepare($sqlInsert);
+
+    $prepared->bindValue(':lastname', $_POST['lastname'], PDO::PARAM_STR);
+    $prepared->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
+    $prepared->bindValue(':civility', $_POST['civility'], PDO::PARAM_INT);
+
+    $prepared->execute();
+
+    echo "INSERTION !!!";
+
+}
+
+
 $sqlContact = 'SELECT * FROM contact ORDER BY lastname, firstname ASC;';
 
 $resultat = $pdo->query($sqlContact);
@@ -18,12 +36,10 @@ $civilityResult = $pdo->query($sqlCivility);
 $dbCivilities = $civilityResult->fetchAll(PDO::FETCH_ASSOC);
 $civilities = [];
 
-foreach ($dbCivilities as $civility){
+foreach ($dbCivilities as $civility) {
 
     $civilities[$civility['id']] = $civility['civility'];
 }
-
-
 
 
 echo '<table>';
@@ -40,18 +56,32 @@ foreach ($contacts as $contact) {
 
     echo '<tr>
         <td>' . $civilities[$contact['civility_id']] . '</td>
-        <td>' . fullname($contact['firstname'], $contact['lastname'])  . '</td>
+        <td>' . fullname($contact['firstname'], $contact['lastname']) . '</td>
     </tr>';
 }
 echo '</tbody>
 </table>';
 
+
 ?>
 
 <form method="post">
-    <label for="firstname"></label>
-    <input type="text" name="firstname" id="firstname">
-    <label for="lastname"></label>
-    <input type="text" name="lastname" id="lastname">
+    <div>
+        <?php
+        foreach ($dbCivilities as $civility) {
+            echo '<input type="radio" name="civility" value="' . $civility['id'] . '"><label>' . $civility['civility'] . '</label>';
+        }
+        ?>
+    </div>
+    <?php
+    $forbidden = ['id', 'civility_id'];
+    foreach ($contacts[0] as $field => $value) {
+        if (!in_array($field, $forbidden)){ ?>
+            <label for="<?= $field; ?>"><?= ucfirst($field); ?></label>
+        <input type="text" name="<?= $field; ?>" id="<?= $field; ?>" placeholder="<?= $value; ?>">
+        <?php
+    }
+    } ?>
 
+    <input type="submit">
 </form>
